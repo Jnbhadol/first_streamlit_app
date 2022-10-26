69feb31d05b24d0afda0509faceb825dc7e599c4
Jnbhadol <90261241+Jnbhadol@users.noreply.github.com>
2022-10-26 10:44:18 -0500

    Update streamlit_app.py
    edited the code to accept edge cases

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,20 +28,22 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.write('The user entered ', fruit_choice)
+  else:
+    #import requests
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    # Normalizing the json file 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # write your own comment - what does this do?
+    streamlit.dataframe(fruityvice_normalized)
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
-# Normalizing the json file 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
-
+except URLERROR as e:
+    streamlit.error()
+    
 #import snowflake.connector
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM fruit_load_list")
